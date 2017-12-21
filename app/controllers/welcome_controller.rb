f6ee77c5b1daf8786591c277b320d66accfa2519
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2017-12-21 02:40:50 +0000

    Added basic controllers&views

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+  # WelcomeController originally contained two (empty) methods: 'index' and 'about'. There have the same names as corresponding views.
+  # (So, inside app/views/welcome there must be a view called 'index')
+  def index
+  end
+
+  def about
+  end
+end
