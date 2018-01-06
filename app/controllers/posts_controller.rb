765795212c94bdb354feb80ebfec7e4cde115773
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2018-01-05 19:31:21 -0600

    Added code for checkpoint18

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,27 @@ class PostsController < ApplicationController
   end
 
   def show
+        @post = Post.find(params[:id])
   end
 
   def new
+      @post = Post.new
+  end
+
+  def create
+      # We call Post.new to create a new instance of Post
+      @post = Post.new
+      @post.title = params[:post][:title]
+      @post.body = params[:post][:body]
+      if @post.save
+          # The flash hash provides a way to pass temporary values between actions. Any value placed in flash will be available in the next action and then deleted
+          flash[:notice] = "Post was saved."
+          # Redirecting to @post will direct the user to the posts 'show' view
+          redirect_to @post
+      else
+          flash.now[:alert] = "There was an error saving the post. Please try again"
+          render :new
+      end
   end
 
   def edit
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -5,43 +5,76 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
 
 #NL: Creates a post and assigns it to my_post using let
-  let (:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
+    let (:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
 
-  describe "GET #index" do
-    it "returns http success" do
-      # the test performs a GET on the index view and expects the response to be successful.
-      get :index
-      # 'have_http_status(:success)' checks for a response code of 200, which is the standard HTTP response code for success
-      expect(response).to have_http_status(:success)
+    describe "GET #index" do
+        it "returns http success" do
+            # the test performs a GET on the index view and expects the response to be successful.
+            get :index
+            # 'have_http_status(:success)' checks for a response code of 200, which is the standard HTTP response code for success
+            expect(response).to have_http_status(:success)
+        end
+
+        it "assigns [my_post] to @posts" do
+            get :index
+            # 'assigns' gives the test access to "instance variables assigned in the action that are available for the view"
+            expect(assigns(:posts)).to eq([my_post])
+        end
+
     end
 
-    it "assigns [my_post] to @posts" do
-        get :index
-        # 'assigns' gives the test access to "instance variables assigned in the action that are available for the view"
-        expect(assigns(:posts)).to eq([my_post])
+    describe "GET new" do
+        it "returns http success" do
+            # When 'new' is invoked, a new and unsaved Post object is created. HTTP convention requires that GET actions (such as new) do not generate (create) new data
+            get :new
+            expect(response).to have_http_status(:success)
+        end
+
+        it "renders the new view" do
+            get :new
+            expect(response).to render_template :new
+        end
+
+        it "instatiates @post" do
+            get :new
+            # 'assigns' gives us access to the @post variable, assigning it to :post
+            expect(assigns(:post)).not_to be_nil
+        end
     end
 
-  end
+    describe "POST create" do
+        it "increases the number of Post by 1" do
+            # When 'create' is invoked, the newly created object is persisted to the database
+            # HTTP convention requires that POST actions are used to create new data
+            # PostsController#create is called with the parameters  {post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}
+            # The 'params' hash contains all parameters passed to the application's controller (application_controller.rb), whether from GET, POST, or any other HTTP action.
+            expect{post :create, params: { post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}}.to change(Post, :count).by(1)
+        end
 
-#  describe "GET #show" do
-#    it "returns http success" do
-#      get :show
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+        it "assigns the new post to @post" do
+            post :create, params: { post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}
+            expect(assigns(:post)).to eq Post.last
+        end
 
-#  describe "GET #new" do
-#    it "returns http success" do
-#      get :new
-#      expect(response).to have_http_status(:success)
-#    end
-# end
+        it "redirects to the new post" do
+            post :create, params: { post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}
+            expect(response).to redirect_to Post.last
+        end
+    end
 
-#  describe "GET #edit" do
-#    it "returns http success" do
-#      get :edit
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+    describe "GET show" do
+        it "returns http success" do
+            get :show, params: { id: my_post.id }
+            expect(response).to have_http_status(:success)
+        end
+        it "renders the #show view" do
+            get :show, params: { id: my_post.id }
+            expect(response).to render_template :show
+        end
+        it "assigns my_post to @post" do
+            get :show, params: { id: my_post.id }
+            expects(assigns(:post)).to eq(my_post)
+        end
+    end
 
 end
