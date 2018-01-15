508959ccc52215300f8f3a6d328f6d1be54994f9
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2018-01-15 10:33:49 -0600

    Added update and destroy methods/views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 #When we use Rails to create a Model, the new class inherits from ApplicationRecord by default
 # ApplicationRecord essentially handles interaction with the database and allows us to persist data through our class
 class Post < ApplicationRecord
-    has_many :comments
+    # In next line, 'dependent: destroy' does a cascade delete
+    has_many :comments, dependent: :destroy
 end
