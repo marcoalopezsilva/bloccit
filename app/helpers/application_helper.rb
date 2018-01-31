3800e8b63086858cfb6f553ce27b65a3199ae356
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2018-01-30 18:22:40 -0600

    Added validation code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #We define a method named 'form_group_tag' which takes two arguments: array of errors, and a block.
+    def form_group_tag(errors, &block)
+      css_class = 'form-group'
+      css_class << ' has-error' if errors.any?
+      #The content_tag takes a symbol argument, a block, and an options hash.
+      #It then creates the symbol-specified HTML tag with the block contents, and if specified, the options.
+      content_tag :div, capture(&block), class: css_class
+     end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ApplicationRecord
     # In next line, 'dependent: destroy' does a cascade delete
     belongs_to :topic
     has_many :comments, dependent: :destroy
+
+    validates :title, length: {minimum: 5}, presence: true
+    validates :body, length: {minimum: 20}, presence: true
+    validates :topic, presence: true
+    
 end
