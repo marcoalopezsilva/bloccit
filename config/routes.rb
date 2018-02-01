2ec3d66d5a4561a5b9afc74eb1e931237becf234
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2018-02-01 16:38:22 -0600

    Partial code for assignment. I get an error message

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
     get 'about' => 'welcome#about'
 
-    post 'confirm' => 'users#confirm'
+    post 'users/confirm' => 'users#confirm'
 
     root "welcome#index"
 end
