6f68d8cc3793d354d3f1c968d1639b45abd220e2
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2018-02-11 21:59:49 -0600

    Coded voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,13 +5,35 @@ class Post < ApplicationRecord
     belongs_to :topic
     belongs_to :user
     has_many :comments, dependent: :destroy
+    # We can see how many votes have been cast on a post by calling post.votes, thanks to the has_many :votes declaration in Post.
+    has_many :votes, dependent: :destroy
 
     # The default_scope will order all posts by their created_at date, in descending order
-    default_scope { order('created_at DESC') }
+    default_scope { order('rank DESC') }
 
     validates :title, length: {minimum: 5}, presence: true
     validates :body, length: {minimum: 20}, presence: true
     validates :topic, presence: true
     validates :user, presence: true
+
+    def up_votes
+      #Here we could have written 'self.votes'
+      #This fetches a collection of votes with a value of 1. We then call count on the collection to get a total of all up votes.
+      votes.where(value: 1).count
+    end
+
+    def down_votes
+      votes.where(value: -1).count
+    end
+
+    def points
+      votes.sum(:value)
+    end
+
+    def update_rank
+     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+     new_rank = points + age_in_days
+     update_attribute(:rank, new_rank)
+   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,12 +21,16 @@ topics = Topic.all
 
 # Create random Posts
 50.times do
-  Post.create!(
+  post = Post.create!(
     user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
+  #NL: creates different "created at dates" so we can use in our ranking algorithm
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  #NL: Assigns random votes to each post. '[-1, 1].sample' randomly selects an up or down vote
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 
 posts = Post.all
